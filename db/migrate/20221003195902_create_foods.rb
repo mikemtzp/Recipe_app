ac4a75bd53a970ad83e7bb7dcd39606d74953a88
Gabriel <gopxfs@hotmail.com>
2022-10-03 17:03:13 -0300

    Add default values to food properties

--- db/migrate/20221003195902_create_foods.rb
+++ db/migrate/20221003195902_create_foods.rb
@@ -1,10 +1,10 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.string :name
-      t.string :measurement_unit
-      t.decimal :price
-      t.integer :quantity
+      t.string :name, default: 'Food'
+      t.string :measurement_unit, default: 'g'
+      t.decimal :price, default: 0
+      t.integer :quantity, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
