7844a576a728c1090d6298188c054e9c1f826571
Gabriel <gopxfs@hotmail.com>
2022-10-03 16:31:15 -0300

    Add default value to user name

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_192340) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "New user"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
