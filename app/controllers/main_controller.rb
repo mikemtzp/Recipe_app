d8c31989a5faab1ad54eb390f1e784aac53cb4b5
Gabriel <gopxfs@hotmail.com>
2022-10-03 16:28:04 -0300

    Remove main page

--- app/controllers/main_controller.rb
+++
@@ -1,5 +0,0 @@
-class MainController < ApplicationController
-  before_action :authenticate_user!
-
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "main#index"
+  root to: "users#index"
 end
