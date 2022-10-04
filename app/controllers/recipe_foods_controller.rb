7b0d3ec3cc2697e425179d456b06e935b566760e
Gabriel <gopxfs@hotmail.com>
2022-10-04 18:50:42 -0300

    Add table to display recipe ingredients

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -54,7 +54,7 @@ class RecipeFoodsController < ApplicationController
     @recipe_food.destroy
 
     respond_to do |format|
-      format.html { redirect_to recipe_foods_url, notice: 'Recipe food was successfully destroyed.' }
+      format.html { redirect_to user_recipe_url }
       format.json { head :no_content }
     end
   end
