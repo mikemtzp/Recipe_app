af9db68a76be59dd65fd1141407e1f9f0b03af60
mikemtzp <mikemp8226@gmail.com>
2022-10-04 11:32:52 -0500

    Modify redirect to url after new recipe is destroyed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -51,7 +51,7 @@ class RecipesController < ApplicationController
     @recipe.destroy
 
     respond_to do |format|
-      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+      format.html { redirect_to user_recipes_url, notice: 'Recipe was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
