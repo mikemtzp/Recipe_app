a20ad7fed26aaf0466097774f85df1190ea3df3a
Gabriel <gopxfs@hotmail.com>
2022-10-04 18:58:11 -0300

    Add condition to doesnt allow empty quantity for ingredients

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,7 +2,7 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
 
-  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   def total_price
     quantity * food.price
