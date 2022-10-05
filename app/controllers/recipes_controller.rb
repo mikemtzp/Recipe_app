2e7968bbc0ceedd25dd06e9f631902533862d9d1
Gabriel <gopxfs@hotmail.com>
2022-10-05 01:33:38 -0300

    Update method on view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,7 @@ class RecipesController < ApplicationController
 
   def shopping_list
     @recipes = current_user.list_recipes
+    @new_ingredients_data = current_user.new_ingredients_information
   end
 
   # GET /recipes/1/edit
