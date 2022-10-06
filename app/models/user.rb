58d0016213a06f7c6854d39ac0a552615347d0f2
Gabriel <gopxfs@hotmail.com>
2022-10-06 15:31:43 -0300

    Add maximum length to string values

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 0, maximum: 50 }
 
   def list_foods
     Food.where(user_id: id)
